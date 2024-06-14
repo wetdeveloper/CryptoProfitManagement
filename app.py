a6816fa26e10772fad1af5a983d92d173d159bc8
wetdeveloper <alexhe1998gerhanov@gmail.com>
2024-06-14 10:44:27 -0700

    initial commit

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-def reinvest(savedprofit):
-    if input(f"You have {savedprofit}$ saved profit.Do you want reinvest your taken profit again?Y/N")=="Y":
-        count=0
+def reinvest(savedprofit,previousPrice,sellprice,stk,autoreinvest=True):#currentPrice=previous price
         for i in range(len(list(stocks_targets))):
-            print(f"{i} {list(stocks_targets)[i]})")
-        reinvestchoice=int(input("what you wanna reinvest?enter the number"))
-        buyprice=float(input("What price you wanna buy?"))
-        investmentAmount=int(input(f"You have {savedprofit} saved profit. how much you wanna invest?enter the amount."))
-        if investmentAmount>0 and investmentAmount<savedprofit:
-            reinvestments.append([list(stocks_targets)[reinvestchoice],buyprice,investmentAmount])
+            print(f"{i} {list(stocks_targets)[i]}")
+        if not(autoreinvest) and input(f"You have {savedprofit}$ saved profit.Do you want reinvest your taken profit again?Y/N:  ")=="Y":
+            print(f"Your previous price level at {stk} is {previousPrice}")
+            reinvestchoice=int(input("what coin do you want to reinvest in?enter the number: "))
+            print(f"Your investing on {list(stocks_targets)[reinvestchoice]}")
+            buyprice=float(input("What price you wanna buy at?"))
+            investmentAmount=float(input(" how much money you wanna invest?enter the amount$: "))
+        else:
+            #auto reinvest
+            reinvestchoice=stk
+            buyprice=previousPrice
+            investmentAmount=savedprofit
+        if investmentAmount>0 and investmentAmount<=savedprofit:
+            if not(autoreinvest):
+                reinvestments.append([list(stocks_targets)[reinvestchoice],buyprice,sellprice,investmentAmount])
+            else:
+                reinvestments.append([reinvestchoice,buyprice,sellprice,investmentAmount,investmentAmount/buyprice*sellprice])#profit is how much we get by buying at previous price and selling at current price
             savedprofit=savedprofit-investmentAmount
-            print("Well done")
+            print("Well done \n")
             return savedprofit
         else:
-            print("lack of ballance")
+            print("lack of ballance \n")
             return 0
-    return 0
-            
-        
-        
-            
 
 
 stocks_targets={
@@ -27,43 +31,48 @@ stocks_targets={
                     'ADA':{'targets':[(2.30,10),(3.5,30),(4.60,50),(6,60),(9,90)],'Savedprofits':[]}
                  }
                  #(target,take profit percentage as reach this target)
-reinvestments=[]#['token','entry price',how much saved profit money']
+                 
+reinvestments=[]#['token','entry price',sell price,reinvested money$']
+
                  
 totallbalance=0                 
 totallsaved=0
+autoreinvest=True
 for stk in stocks_targets:
-                stk_quantity=int(input(f"How many {stk} do you have"))
+                stk_quantity=int(input(f"How many {stk} do you have: "))
                 print(f"Stock:{stk}")
                 for i in range(0,len(stocks_targets[stk]['targets'])):
                     savepercentage=stocks_targets[stk]['targets'][i][1]
                     if i==0:
                         balance=stk_quantity*stocks_targets[stk]['targets'][i][0]
                         savedprofit=balance*savepercentage/100
-                        #savedprofit-=reinvest(savedprofit)
                         totallsaved+=savedprofit
-                        
                         stocks_targets[stk]['Savedprofits'].append(savedprofit)
                         balance=balance-savedprofit
                         print(f"TP{i+1}:{stocks_targets[stk]['targets'][i][0]} ---- Balance:{balance}--------Save Percentage:{savepercentage}----- Saved profit:{stocks_targets[stk]['Savedprofits'][i]}")
-                        totallsaved+=stocks_targets[stk]['Savedprofits'][i]
+                        print(f"{stk} totall saved:{sum(stocks_targets[stk]['Savedprofits'])}")
+                        print(f"Totall saved profit:{totallsaved}")
+                        print("----------------------------------------------------------------------------------------------------------------")
                         continue
-                    balance=(balance/stocks_targets[stk]['targets'][i-1][0])*(stocks_targets[stk]['targets'][i][0])
+                    previousPrice=stocks_targets[stk]['targets'][i-1][0]
+                    balance=(balance/previousPrice)*(stocks_targets[stk]['targets'][i][0])
                     savedprofit=balance*savepercentage/100
-                    #savedprofit-=reinvest(savedprofit)
-                    stocks_targets[stk]['Savedprofits'].append(savedprofit)
-                    #totallsaved+=stocks_targets[stk]['Savedprofits'][i]
-                    totallsaved+=savedprofit
                     balance=float(balance-savedprofit)
+                    savedprofit=reinvest(savedprofit,previousPrice,stocks_targets[stk]['targets'][i][0],stk)
+                    balance+=savedprofit
+                    print(f"savedprofit after reinvesting:{savedprofit}")
+                    stocks_targets[stk]['Savedprofits'].append(savedprofit)
+                    totallsaved+=savedprofit
                     print(f"TP{i+1}:{stocks_targets[stk]['targets'][i][0]} ---- Balance:{balance}--------Save Percentage:{savepercentage}------ Saved profit:{stocks_targets[stk]['Savedprofits'][i]}")
-                print(f"{stk} totall saved:{sum(stocks_targets[stk]['Savedprofits'])}")
+                    print(f"{stk} totall saved:{sum(stocks_targets[stk]['Savedprofits'])}")
+                    print(f"Totall saved profit:{totallsaved}")
+                    print("----------------------------------------------------------------------------------------------------------------------------")
                 totallbalance+=balance
-                        
-     
-#totallbalance=totallsaved-totalbalance
+                
 print(f"reinvestments:{reinvestments}")
+totallbalance+=sum([item[4] for item in reinvestments])
 print(f"totalsaved:{totallsaved}")
 print(f"Totall Balance:{totallbalance}$")
-print(f"Totall Balance:{totallbalance*60000}rial")
                     
                     
                 
