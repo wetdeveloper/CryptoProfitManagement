020d916466da7ec15cba45f164eaf3ba307a05a2
wetdeveloper <alexhe1998gerhanov@gmail.com>
2024-06-17 08:41:20 -0700

    initial commit

--- app.py
+++ app.py
@@ -1,43 +1,47 @@
-def reinvest(savedprofit,previousPrice,sellprice,stk,autoreinvest=True):#currentPrice=previous price
+def reinvest(savedprofit,previousPrice,sellprice,stk,currentprice,autoreinvest=True):#currentPrice=previous price
         for i in range(len(list(stocks_targets))):
             print(f"{i} {list(stocks_targets)[i]}")
-        if not(autoreinvest) and input(f"You have {savedprofit}$ saved profit.Do you want reinvest your taken profit again?Y/N:  ")=="Y":
-            print(f"Your previous price level at {stk} is {previousPrice}")
-            reinvestchoice=int(input("what coin do you want to reinvest in?enter the number: "))
-            print(f"Your investing on {list(stocks_targets)[reinvestchoice]}")
-            buyprice=float(input("What price you wanna buy at?"))
-            investmentAmount=float(input(" how much money you wanna invest?enter the amount$: "))
-        else:
-            #auto reinvest
+        print(f"currentPrice is {currentprice}")
+        if not (autoreinvest):
+            if input(f"You have {savedprofit}$ saved profit.Do you want reinvest your taken profit again?Y/N:  ")=="Y":
+                print(f"Your previous price level at {stk} is {previousPrice}")
+                reinvestchoice=int(input("what coin do you want to reinvest in?enter the number: "))
+                print(f"Your investing on {list(stocks_targets)[reinvestchoice]}")
+                buyprice=float(input("What price you wanna buy at?"))
+                investmentAmount=float(input(" how much money you wanna invest?enter the amount$: "))
+            else:
+                print("You did not reinvest.")
+                return savedprofit
+        elif autoreinvest:   
             reinvestchoice=stk
             buyprice=previousPrice
             investmentAmount=savedprofit
         if investmentAmount>0 and investmentAmount<=savedprofit:
             if not(autoreinvest):
-                reinvestments.append([list(stocks_targets)[reinvestchoice],buyprice,sellprice,investmentAmount])
+                reinvestments.append([list(stocks_targets)[reinvestchoice],buyprice,sellprice,investmentAmount,investmentAmount/buyprice*sellprice])
             else:
                 reinvestments.append([reinvestchoice,buyprice,sellprice,investmentAmount,investmentAmount/buyprice*sellprice])#profit is how much we get by buying at previous price and selling at current price
             savedprofit=savedprofit-investmentAmount
             print("Well done \n")
             return savedprofit
         else:
-            print("lack of ballance \n")
-            return 0
+            print("lack of ballance.try at next target again.you didn't use your saved profit\n")
+            return savedprofit
 
 
 stocks_targets={
 
-                    'Alice':{ 'targets':[(4.968,20),(8.37,30),(14.89,50),(20.22,70),(32.696,80),(45,80),(65,100)],'Savedprofits':[],},
-                    'ADA':{'targets':[(2.30,10),(3.5,30),(4.60,50),(6,60),(9,90)],'Savedprofits':[]}
+                    'Alice':{ 'targets':[(4.20,10),(8,10),(14,50),(20,30),(30.696,50),(40,50),(65,80),(70,100)],'Savedprofits':[],},
+                    'ADA':{'targets':[(2.30,20),(3.5,0),(4.60,60),(6,80),(9,90)],'Savedprofits':[]}
                  }
                  #(target,take profit percentage as reach this target)
                  
 reinvestments=[]#['token','entry price',sell price,reinvested money$']
 
-                 
+reinvestmentactive=False  #the way you reinvest             
 totallbalance=0                 
 totallsaved=0
-autoreinvest=True
+autoreinvest=True #if false=>reinvest manualy
 for stk in stocks_targets:
                 stk_quantity=int(input(f"How many {stk} do you have: "))
                 print(f"Stock:{stk}")
@@ -58,21 +62,25 @@ for stk in stocks_targets:
                     balance=(balance/previousPrice)*(stocks_targets[stk]['targets'][i][0])
                     savedprofit=balance*savepercentage/100
                     balance=float(balance-savedprofit)
-                    savedprofit=reinvest(savedprofit,previousPrice,stocks_targets[stk]['targets'][i][0],stk)
-                    balance+=savedprofit
+                    currentprice=stocks_targets[stk]['targets'][i][0]
+                    if reinvestmentactive:
+                        savedprofit=reinvest(savedprofit,previousPrice,stocks_targets[stk]['targets'][i][0],stk,currentprice,autoreinvest)
+                        #balance+=savedprofit
                     print(f"savedprofit after reinvesting:{savedprofit}")
                     stocks_targets[stk]['Savedprofits'].append(savedprofit)
                     totallsaved+=savedprofit
-                    print(f"TP{i+1}:{stocks_targets[stk]['targets'][i][0]} ---- Balance:{balance}--------Save Percentage:{savepercentage}------ Saved profit:{stocks_targets[stk]['Savedprofits'][i]}")
+                    print(f"TP{i+1}:{stocks_targets[stk]['targets'][i][0]} ---- Balance:{balance}--------Save Percentage:{savepercentage}------ Saved profit:{savedprofit}")
                     print(f"{stk} totall saved:{sum(stocks_targets[stk]['Savedprofits'])}")
                     print(f"Totall saved profit:{totallsaved}")
                     print("----------------------------------------------------------------------------------------------------------------------------")
                 totallbalance+=balance
                 
 print(f"reinvestments:{reinvestments}")
+reinvestmentsmade=sum([item[4] for item in reinvestments])
+print(f"reinvestment made money:{reinvestmentsmade}$")
 totallbalance+=sum([item[4] for item in reinvestments])
 print(f"totalsaved:{totallsaved}")
-print(f"Totall Balance:{totallbalance}$")
+print(f"Totall Balance(Balance+reinvestment made money+totall saved profit:{totallbalance+totallsaved}$")
                     
                     
                 
@@ -81,3 +89,4 @@ print(f"Totall Balance:{totallbalance}$")
                     
                     
                     
+#باید یه ابشنی اضافه کنم که مثلا هنگامی که میخوام اتوماتیک سیو سود کنه چند درصد بایین تر از قیمتی که فروخته بخره.
